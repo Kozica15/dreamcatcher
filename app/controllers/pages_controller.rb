686dc332341b738acb0cdec2d6b69f41f220a981
claire fumey <fumey_claire@hotmail.com>
2018-07-29 17:43:49 +0200

    createdevise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
-
+# saute=skip saute le before action seulement pour page home
   def home
   end
 end
