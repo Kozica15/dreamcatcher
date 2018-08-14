59beddce04d240ce544c01da4ae82f8754ddd21d
claire fumey <fumey_claire@hotmail.com>
2018-08-14 09:21:07 +0200

    new

---
+++ app/controllers/dreams_controller.rb
@@ -0,0 +1,11 @@
+class DreamsController < ApplicationController
+
+  def create
+  end
+  def index
+  end
+  def new
+    @dream = Dream.new
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :dreams
+  resources :dreams, only: [:new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
